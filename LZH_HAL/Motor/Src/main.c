c5fa7fb16aeb7d30b1a6376b4283f3dbe690cbc8
Lzih7 <2182197114@qq.com>
2025-11-27 14:43:11 +0800

    modify Motor project

--- LZH_HAL/Motor/Src/main.c
+++ LZH_HAL/Motor/Src/main.c
@@ -10,7 +10,6 @@
 // #include "UART_Set.h"
 
 TIM_HandleTypeDef htim3;
-TIM_HandleTypeDef htim5;
 uint16_t PWM_IN_Wid[4];
 
 // HAL库时钟配置函数 - HSE + 84MHz系统时钟 + 48MHz USB
@@ -54,14 +53,13 @@ int main(void)
     OLED_Init();
     IC_Init();
     BLDC_Init();
-    OLED_ShowString(1, 1, "BLDC Init");
     // Calibrate_BLDC();
     // OLED_ShowString(1, 1, "BLDC Calibrated");
     
     while (1)
     {
-        OLED_ShowNum(2, 1, PWM_IN_Wid[2], 4);
-        // BLDC_SetThrottle3_us(PWM_IN_Wid[2]);
+        // OLED_ShowNum(2, 1, PWM_IN_Wid[2], 4);
+        // BLDC_SetThrottle_us(1500, 3);
     }
 }
 
